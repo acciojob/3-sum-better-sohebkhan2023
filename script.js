6a548502cb161db7a6dbfa2e3c98ab89143e17db
Sohebkhan <102939844+sohebkhan2023@users.noreply.github.com>
2023-09-09 20:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function threeSum(arr, target) {
+function threeSum(arr, target) 
+{
 // write your code here
+	for(let i=0;i<arr.length-2;i++)
+	{   let sum=0;
+		for(let j=i+1;j<arr.length-1;j++)
+		{
+			for(let k=j+1;k<arr.length;k++)
+			{
+				sum=arr[i]+arr[j]+arr[k];
+				if(sum==target)
+				break;	
+				
+			}
+		}
+	}
+	return sum;
   
 }
 
